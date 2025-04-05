2a46681ec16705b24a4b6dff0a206e78427b3502
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-05 17:08:57 +0530

    feat: contact details entity model

---
+++ Alumni_Portal.API/EntityModels/Models/ContactDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace EntityModels.Models
+{
+    public class ContactDetails
+    {
+        public Guid ContactDetailsId { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string SocialMediaType { get; set; }
+        public string SocialMediaURL { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid? LastUpdatedBy { get; set; }
+        public DateTime LastUpdatedOn { get; set; }
+    }
+}
