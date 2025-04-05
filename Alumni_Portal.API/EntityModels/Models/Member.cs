52d4c7d3a4e4ecd0baf7fb20c3ecbec1754968a1
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-05 16:57:32 +0530

    feat: member entity model

---
+++ Alumni_Portal.API/EntityModels/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace EntityModels.Models
+{
+    public class Member
+    {
+        public Guid MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedOn { get; set; }
+    }
+}
