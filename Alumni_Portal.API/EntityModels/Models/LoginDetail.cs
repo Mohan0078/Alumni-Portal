ebe9de3b3e15ed6a42892a45fdac73513fd49b3d
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-06 16:20:48 +0530

    feat: login detail entity model

---
+++ Alumni_Portal.API/EntityModels/Models/LoginDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityModels.Models
+{
+    internal class LoginDetail
+    {
+      public Guid LoginDetailId { get; set; }
+      public Guid MemberId { get; set; }
+      public string LoginEmail { get; set; }
+      public string Password { get; set; }
+      public virtual Member Member { get; set; }
+    }
+}
