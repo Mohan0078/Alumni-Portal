2b61c5168e8b8604569b3d472f6dc11e7c9bd399
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-06 15:31:03 +0530

    feat: address entity model

---
+++ Alumni_Portal.API/EntityModels/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace EntityModels.Models
+{
+    public class Address
+    {
+      public Guid AddressId { get; set; }
+      public string StreetAddress { get; set; }
+      public Guid CityId { get; set; }
+      public Guid StateId { get; set; }
+      public string ZipCode { get; set; }
+      public Guid CreatedBy { get; set; }
+      public DateTime CreatedOn { get; set; }
+      public Guid? ModifiedBy { get; set; }
+      public DateTime? ModifiedOn { get; set; }
+    }
+}
