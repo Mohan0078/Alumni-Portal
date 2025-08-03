742aa3e99184417381f10047ad9924f27cdb5766
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-08-03 19:33:05 +0530

    feat: add member request model

---
+++ Alumni_Portal.API/Alumni_Portal.Models/RequestModels/AddMemberRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumni_Portal.Models.RequestModels
+{
+    public class AddMemberRequestModel
+    {
+      public string FirstName { get; set; }
+      public string MiddleName { get; set; }
+      public string LastName { get; set; }
+      public string Gender { get; set; }
+      public string Email { get; set; }
+    }
+}
