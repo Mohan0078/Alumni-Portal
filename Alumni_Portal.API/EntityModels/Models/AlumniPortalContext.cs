ad4cca6a36a53b57e2df826dba53ea600c490868
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-08-17 17:00:50 +0530

    feat: alumni portal context

--- Alumni_Portal.API/EntityModels/Models/AlumniPortalContext.cs
+++ Alumni_Portal.API/EntityModels/Models/AlumniPortalContext.cs
@@ -4,6 +4,13 @@ namespace EntityModels.Models
 {
     public class AlumniPortalContext : DbContext
     {
-      
+        public AlumniPortalContext(DbContextOptions<AlumniPortalContext> options) : base(options)
+        {
+        }
+        public DbSet<Address> Addresses;
+        public DbSet<ContactDetails> ContactDetails;
+        //public DbSet<LoginDetails> LoginDetails;
+        public DbSet<Member> Members;
+        public DbSet<MemberAddress> MemberAddresses;
     }
 }
