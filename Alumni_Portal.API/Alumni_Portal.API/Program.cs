e7186e5758ff47e5888e7bb95dd1d6b30dbbdea4
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-12-16 22:21:00 +0530

    feat: adding jwt to middleware

--- Alumni_Portal.API/Alumni_Portal.API/Program.cs
+++ Alumni_Portal.API/Alumni_Portal.API/Program.cs
@@ -1,5 +1,8 @@
 using EntityModels.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(options =>
+       {
+           options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+           {
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidateLifetime = true,
+               ValidateIssuerSigningKey = true,
+               ValidIssuer = "",
+               ValidAudience = "",
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(""))
+           };
+       });
 
 builder.Services.AddDbContext<AlumniPortalContext>(options => 
                        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
