7b81529f4a43558479bca4f6ea583f8a9ba4feb9
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-12-16 22:26:01 +0530

    feat: AuthenticationService with GenerateToken method

---
+++ Alumni_Portal.API/Alumni_Portal.Services/Services/AuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Alumni_Portal.Services.Interfaces;
+
+namespace Alumni_Portal.Services.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public async Task<string> GenerateToken(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
