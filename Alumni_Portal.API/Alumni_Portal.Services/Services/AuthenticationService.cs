3a694f62bab90e63a09266ecbe423b12e1df0bbe
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-12-17 19:15:05 +0530

    feat: Configuration in AuthenticationService

--- Alumni_Portal.API/Alumni_Portal.Services/Services/AuthenticationService.cs
+++ Alumni_Portal.API/Alumni_Portal.Services/Services/AuthenticationService.cs
@@ -1,9 +1,17 @@
 ﻿using Alumni_Portal.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace Alumni_Portal.Services.Services
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<string> GenerateToken(string userName)
         {
             throw new NotImplementedException();
