e011f5d656bd170dac25617fd6b13f1b8acbbfa8
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-12-20 16:10:24 +0530

    feat: IUnitOfWork

---
+++ Alumni_Portal.API/Alumni_Portal.Services/Utilities/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alumni_Portal.Services.Utilities
+{
+    internal interface IUnitOfWork<out TContext> where TContext : DbContext, new()
+    {
+        TContext Context { get; }
+    }
+}
