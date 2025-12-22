52b2418d381bd939407a0ae3d0f806d7ad7d9e4e
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-12-22 16:59:16 +0530

    feat: unit of work

---
+++ Alumni_Portal.API/Alumni_Portal.Services/Utilities/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using EntityModels.Models;
+
+namespace Alumni_Portal.Services.Utilities
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AlumniPortalContext _alumniPortalContext;
+
+        public UnitOfWork(AlumniPortalContext alumniPortalContext)
+        {
+            _alumniPortalContext = alumniPortalContext;
+        }
+
+        public Task<int> CommitTransactionAsync()
+        {
+            return _alumniPortalContext.SaveChangesAsync();
+        }
+    }
+}
