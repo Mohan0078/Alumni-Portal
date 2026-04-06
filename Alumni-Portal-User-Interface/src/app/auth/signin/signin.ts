a5872aced2c84edb0de81b0c41ae238dca01c3eb
Mohan Patel <mohanpatel.mp73@gmail.com>
2026-04-06 22:31:04 +0530

    signin component

--- Alumni-Portal-User-Interface/src/app/auth/signin/signin.ts
+++ Alumni-Portal-User-Interface/src/app/auth/signin/signin.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Authentication } from '../../services/authentication';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
-  imports: [ReactiveFormsModule],
   templateUrl: './signin.html',
-  styleUrl: './signin.scss',
+  styleUrls: ['./signin.scss'],
+  imports: [ReactiveFormsModule, CommonModule]
 })
-
-
 export class Signin {
   signinForm: FormGroup;
   submitted = false;
+  loading = false;
+  errorMsg = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private auth: Authentication) {
     this.signinForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      rememberMe: [false]
+      password: ['', [Validators.required]]
     });
   }
 
@@ -27,13 +28,28 @@ export class Signin {
 
   onSubmit() {
     this.submitted = true;
+    this.errorMsg = '';
 
-    if (this.signinForm.invalid) {
-      return;
-    }
+    if (this.signinForm.invalid) return;
 
-    console.log('Form Data:', this.signinForm.value);
+    this.loading = true;
 
-    // TODO: Connect with API
+    this.auth.login(this.signinForm.value).subscribe({
+      next: (res) => {
+        console.log('Login success:', res);
+
+        // Example: store token
+        localStorage.setItem('token', res);
+
+        this.loading = false;
+
+        // TODO: redirect to dashboard
+      },
+      error: (err) => {
+        console.error('Login error:', err);
+        this.errorMsg = err.error?.message || 'Login failed';
+        this.loading = false;
+      }
+    });
   }
 }
