3911a72078bbfe662b454f463b23c3fbf85d9ec6
Mohan Patel <mohanpatel.mp73@gmail.com>
2026-05-03 18:09:49 +0530

    app routes for navigation

--- Alumni-Portal-User-Interface/src/app/app.routes.ts
+++ Alumni-Portal-User-Interface/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Signin } from './auth/signin/signin';
+import { Dashboard } from './dashboard/dashboard';
+import { AppLayout } from './layout/app-layout/app-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+// Auth routes (no layout)
+  {
+    path: 'login',
+    component: Signin
+  },
+  // App routes (with sidebar + topbar)
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+    //   { path: 'alumni', component: AlumniComponent },
+    //   { path: 'jobs', component: JobsComponent },
+    //   { path: 'events', component: EventsComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }
+];
