72427e35cbb95c07f18b8ec38224795d8ebd2eb5
Mohan Patel <mohanpatel.mp73@gmail.com>
2026-05-03 18:02:43 +0530

    app component

--- Alumni-Portal-User-Interface/src/app/app.ts
+++ Alumni-Portal-User-Interface/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Signin } from "./auth/signin/signin";
+import { Sidebar } from "./sidebar/sidebar";
+import { Topbar } from "./topbar/topbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Signin],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
